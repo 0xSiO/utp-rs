d1525feb74036974c918ec2f5c0a8291cf82d6a2
Luc Street <6709262+0xSiO@users.noreply.github.com>
2020-08-29 18:41:58 -0700

    Note a possible source of future bug

--- src/listener.rs
+++ src/listener.rs
@@ -124,7 +124,16 @@ impl ConnectionManager {
     }
 
     pub fn set_state(&self, id: u16, state: ConnectionState) -> bool {
-        self.connection_states.insert(id, state)
+        if self.connection_states.contains_key(&id) {
+            false
+        } else {
+            // TODO: This could cause a bug where state is mysteriously overridden
+            //       If this is a problem, consider using Arc<Mutex<HashMap>>
+            let success = self.connection_states.insert(id, state);
+            // Make absolutely sure the state didn't already exist
+            debug_assert!(success);
+            true
+        }
     }
 
     pub fn route(&self, packet: Packet) {
