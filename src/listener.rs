3adc5a6b49ceb05076d30877842cd3c960a16b4d
Luc Street <6709262+0xSiO@users.noreply.github.com>
2021-03-28 00:30:10 -0700

    Update docs for UtpListener, remove extraneous loop

--- src/listener.rs
+++ src/listener.rs
@@ -19,55 +19,61 @@ impl UtpListener {
         Self { socket }
     }
 
-    /// Creates a new UtpListener, which will be bound to the specified address.
+    /// Create a new UDP socket and attempt to bind it to the provided address.
     pub async fn bind(addr: impl ToSocketAddrs) -> Result<Self> {
         Ok(Self::new(Arc::new(UtpSocket::bind(addr).await?)))
     }
 
+    /// Return the local address that this socket is bound to.
     pub fn local_addr(&self) -> SocketAddr {
         self.socket.local_addr()
     }
 
+    /// Accept a connection from a remote socket, returning a [`UtpStream`].
+    ///
+    /// This will first wait for a `Syn` packet from the socket, then initialize a connection in
+    /// the socket's routing table. If that is successful, a `State` packet will be sent back to
+    /// the remote socket and a new stream will be created.
     pub async fn accept(&self) -> Result<UtpStream> {
-        loop {
-            let (packet, remote_addr) = self.socket.get_syn().await?;
-            let connection_id_recv = packet.connection_id.wrapping_add(1);
-            let connection_id_send = packet.connection_id;
-            let seq_number = rand::random::<u16>();
-            let ack_number = packet.seq_number;
+        let (packet, remote_addr) = self.socket.get_syn().await?;
+        let connection_id_recv = packet.connection_id.wrapping_add(1);
+        let connection_id_send = packet.connection_id;
+        let seq_number = rand::random::<u16>();
+        let ack_number = packet.seq_number;
 
-            // state: SYN received
+        // state: SYN received
 
-            if self
-                .socket
-                .init_connection(connection_id_recv, remote_addr)
-                .is_ok()
-            {
-                #[rustfmt::skip]
+        if self
+            .socket
+            .init_connection(connection_id_recv, remote_addr)
+            .is_ok()
+        {
+            #[rustfmt::skip]
                 let syn_ack = Packet::new(
                     PacketType::State, 1, connection_id_send, 0, 0, 0, seq_number, ack_number,
                     vec![], Bytes::new(),
                 );
-                let seq_number = seq_number.wrapping_add(1);
-                self.socket.send_to(syn_ack, remote_addr).await?;
+            let seq_number = seq_number.wrapping_add(1);
+            self.socket.send_to(syn_ack, remote_addr).await?;
 
-                // TODO: We aren't technically 'connected' until we start receiving data packets
+            // TODO: We aren't technically 'connected' until we start receiving data packets
 
-                return Ok(UtpStream::new(
-                    Arc::clone(&self.socket),
-                    connection_id_recv,
-                    connection_id_send,
-                    remote_addr,
-                    seq_number,
-                    ack_number,
-                    Default::default(),
-                    Default::default(),
-                    Default::default(),
-                    Default::default(),
-                ));
-            } else {
-                todo!("Failed to initialize connection. Perhaps one exists already in the routing table?");
-            }
+            return Ok(UtpStream::new(
+                Arc::clone(&self.socket),
+                connection_id_recv,
+                connection_id_send,
+                remote_addr,
+                seq_number,
+                ack_number,
+                Default::default(),
+                Default::default(),
+                Default::default(),
+                Default::default(),
+            ));
+        } else {
+            todo!(
+                "Failed to initialize connection. Perhaps one exists already in the routing table?"
+            );
         }
     }
 }
