ebd5a92bf9af44080569a606e1280ce5e74a6678
Luc Street <6709262+0xSiO@users.noreply.github.com>
2022-06-01 20:26:00 -0700

    Expand docs for CongestionController, use ethernet MTU as initial remote receive window

--- src/congestion.rs
+++ src/congestion.rs
@@ -1,20 +1,25 @@
 use crate::{packet::Packet, time::current_micros};
 
 /// This is the initial receive buffer capacity used by libtorrent
-pub const LOCAL_RECEIVE_WINDOW_SIZE: u32 = 1024 * 1024;
+const LOCAL_RECEIVE_WINDOW_SIZE: u32 = 1024 * 1024;
+const ETHERNET_MTU: u32 = 1500;
 
 #[allow(dead_code)]
 pub struct CongestionController {
     /// Number of bytes currently in-flight (sent but not yet ACKed)
     current_window: u32,
-    /// Maximum number of bytes the local socket can currently receive
+    /// Maximum number of bytes we can currently receive.
+    /// This is advertised to the remote connection in each packet we send.
     local_recieve_window: u32,
-    /// Maximum number of bytes the remote socket can currently receive
+    /// Maximum number of bytes they can currently receive.
+    /// We always want to keep current_window below this value.
     remote_receive_window: u32,
-    /// Local lowest-ever seen timestamp difference in microseconds
+    /// Our lowest-ever seen timestamp difference in microseconds
     local_base_delay: u32,
-    /// Remote lowest-ever seen timestamp difference in microseconds
+    /// Their lowest-ever seen timestamp difference in microseconds
     remote_base_delay: u32,
+
+    // TODO: Maybe make these maps of Instant -> delay sample
     /// Differences between received packet timestamp delay and the local base delay
     local_delay_samples: Vec<u32>,
     /// Differences between sent packet timestamp delay and the remote base delay
@@ -26,7 +31,8 @@ impl CongestionController {
         Self {
             current_window: 0,
             local_recieve_window: LOCAL_RECEIVE_WINDOW_SIZE,
-            remote_receive_window: 0,
+            // This is what libtorrent uses, which should let us send at least 1 packet to start
+            remote_receive_window: ETHERNET_MTU,
             local_base_delay: u32::MAX,
             remote_base_delay: u32::MAX,
             local_delay_samples: vec![],
@@ -35,7 +41,6 @@ impl CongestionController {
     }
 
     pub fn update_state(&mut self, received_packet: &Packet) {
-        // TODO: Double check that underflow isn't going to do anything weird
         let local_delay = current_micros().wrapping_sub(received_packet.timestamp_micros);
         let remote_delay = received_packet.timestamp_delta_micros;
 
